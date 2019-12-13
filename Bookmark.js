9ed909ba46a65844e7499c68e233ce95712f19de
Seven Yu <dofyyu@gmail.com>
2019-12-13 14:09:56 +0800

    替换 http https 为 request 包，支持检测页面编码

--- Bookmark.js
+++ Bookmark.js
@@ -1,70 +1,66 @@
-const http = require('http')
-const https = require('https')
+const request = require('request')
+const iconv = require('iconv-lite')
 
-function url2id(url) {
-  return url.replace(/\W/gi, '')
-}
+const CHARSET = 'utf-8'
 
 module.exports = {
   create(url, callback) {
-    let proxy
-    if (url.search(/^https/i) !== -1) {
-      proxy = https
-    } else {
-      proxy = http
-    }
-    proxy.get(
-      url,
+    request(
       {
-        headers: { 'User-Agent': 'Mozilla/5.0' },
+        url,
+        gzip: true,
+        method: 'GET',
+        encoding: null,
+        headers: {
+          'User-Agent': 'Mozilla/5.0',
+        },
       },
-      res => {
+      (error, res, rawData) => {
         const { statusCode } = res
         const contentType = res.headers['content-type']
-        let error
+
         if (statusCode >= 400) {
           error = new Error(`Request Failed.\nStatus Code: ${statusCode}`)
-        } else if (!/^text\/html/.test(contentType)) {
+        } else if (!/^text\/html/i.test(contentType)) {
           error = new Error(
             `Invalid content-type.\nExpected text/html but received ${contentType}`,
           )
         }
         if (error) {
-          // Consume response data to free up memory
-          res.resume()
           callback(error, null)
         } else {
-          res.setEncoding('utf8')
-          let rawData = ''
-          res.on('data', chunk => {
-            rawData += chunk
+          let testString = rawData.toString()
+
+          // get charset
+          let charsetReg = /<\s*meta[^>]+charset=['"](.*?)['"]/gi
+          let charsetMatch = charsetReg.exec(testString)
+          let charset = charsetMatch ? charsetMatch[1].toLowerCase() : CHARSET
+
+          if (charset !== CHARSET) {
+            testString = iconv.decode(rawData, charset)
+          }
+
+          // get title and keywords
+          let titleReg = /<\s*title[^>]*>(.*?)<\s*\/\s*title>/gi
+          let keywordsReg = /<\s*meta[^>]+name=['"]keywords['"][^>]+content=['"](.*?)['"]/gi
+          let descReg = /<\s*meta[^>]+name=['"]description['"][^>]+content=['"](.*?)['"]/gi
+
+          let titleMatch = titleReg.exec(testString)
+          let keywordsMatch = keywordsReg.exec(testString)
+          let descMatch = descReg.exec(testString)
+
+          let title = titleMatch ? titleMatch[1] : url
+          let keywords = keywordsMatch ? keywordsMatch[1] : ''
+          let description = (descMatch ? descMatch[1] : url) || url
+          callback(null, {
+            _id: `bmaw::${url}`,
+            title,
+            keywords,
+            description,
+            url,
+            times: 0,
+            search: [url, title, keywords, description].join(),
           })
-          res
-            .on('end', () => {
-              // TODO: get title and keywords
-              let titleReg = /<\s*title[^>]*>(.*?)<\s*\/\s*title>/gi
-              let keywordsReg = /<\s*meta[^>]+name=['"]keywords['"][^>]+content=['"](.*?)['"]/gi
-              let descReg = /<\s*meta[^>]+name=['"]description['"][^>]+content=['"](.*?)['"]/gi
-              let titleMatch = titleReg.exec(rawData)
-              let keywordsMatch = keywordsReg.exec(rawData)
-              let descriptionMatch = descReg.exec(rawData)
-              let title = titleMatch ? titleMatch[1] : url
-              let keywords = (keywordsMatch ? keywordsMatch[1] : url) || url
-              let description =
-                (descriptionMatch ? descriptionMatch[1] : url) || url
-              callback(null, {
-                _id: url2id(url),
-                title,
-                keywords,
-                description,
-                url,
-                times: 0,
-                search: [url, title, keywords, description].join(),
-              })
-            })
-            .on('error', error => {
-              callback(error, null)
-            })
         }
       },
     )
